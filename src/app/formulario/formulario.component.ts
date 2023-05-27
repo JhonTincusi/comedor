51d9a67b831baac6f54b28c7820229b41a89f21b
JhonTincusi <ajhonjtc@gmail.com>
2023-05-27 17:13:32 -0500

    commit

---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent implements OnInit {
+  alumno = {
+    codigo: '',
+    password: '',
+    captchaInput: ''
+  }
+
+  /**Procesar formulario */
+  procesar(){
+    
+  }
+
+  /**Codigo captcha*/
+  captcha: string = '';
+
+  
+
+  ngOnInit(): void {
+    this.generateCaptcha();
+  }
+
+  generateCaptcha(): void {
+    this.captcha = this.generateRandomText(4);
+  }
+
+  generateRandomText(length: number): string {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(randomIndex);
+    }
+
+    return result;
+  }
+
+
+  /**Codigo para contador de cupos */
+  contador: number = 1200;
+
+  /**Disminuid contador de cupos al darle click al boton reservar */
+  reservar(): void {
+    if (this.contador > 0 && this.alumno.codigo !== '' && this.alumno.password !== '' && this.alumno.captchaInput !== '') {
+      this.contador--;
+    }
+  }
+
+
+
+  
+  
+}
